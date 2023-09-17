3734ddaee9c228970fe022cb5c363dfee5c72ea0
Andres Court <andres@andrescourt.com>
2023-09-17 12:13:34 -0500

    REFACTOR: move the routing logic to the api module

---
+++ api/routes.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/bca-go/models"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+type Router struct {
+	db      models.DB
+	r       *mux.Router
+	Handler http.Handler
+}
+
+func (b *Router) Routes() {
+	b.db = models.Initialize()
+	b.r = mux.NewRouter()
+
+	b.r.Use(jsonMiddleware) // All responses will be of type application/json
+
+	b.r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		res := response{
+			Message: "Testing again and again",
+		}
+		json.NewEncoder(w).Encode(res)
+	})
+
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
+
+	b.Handler = handlers.CORS(originsOk, methodsOk)(b.r)
+}
