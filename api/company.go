a92831ccdb7f68917e7e48f2f15aa713c48b3739
Andres Court <andres@andrescourt.com>
2023-09-17 12:49:05 -0500

    Changed the register method to post and validate body

--- api/company.go
+++ api/company.go
@@ -5,15 +5,26 @@ import (
 	"net/http"
 )
 
+type registerCompany struct {
+	Id           string `json:"id"`
+	Name         string `json:"name"`
+	Employees    uint   `json:"employees"`
+	UserName     string `json:"user_name"`
+	UserPassword string `json:"user_password"`
+	UserFullName string `json:"user_full_name"`
+}
+
 func register(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	res := response{
-		Message: "This is the register route",
+	var newCompany registerCompany
+	err := json.NewDecoder(r.Body).Decode(&newCompany)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
-	json.NewEncoder(w).Encode(res)
+
+	json.NewEncoder(w).Encode("success")
 }
 
 func (b *Router) companyRoutes() {
-	b.r.HandleFunc("/register", register)
-
+	b.r.HandleFunc("/register", register).Methods("POST")
 }
