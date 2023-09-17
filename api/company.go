69230b072bf3b07bb46e1e45c78ef2620b4593dc
Andres Court <andres@andrescourt.com>
2023-09-17 13:04:02 -0500

    on register save the company info

--- api/company.go
+++ api/company.go
@@ -3,6 +3,10 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/alcb1310/bca-go/models"
+	"gitlab.com/0x4149/logz"
+	"gorm.io/gorm"
 )
 
 type registerCompany struct {
@@ -14,6 +18,8 @@ type registerCompany struct {
 	UserFullName string `json:"user_full_name"`
 }
 
+var database *gorm.DB
+
 func register(w http.ResponseWriter, r *http.Request) {
 	var newCompany registerCompany
 	err := json.NewDecoder(r.Body).Decode(&newCompany)
@@ -22,9 +28,25 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	json.NewEncoder(w).Encode("success")
+	c := models.Company{
+		Ruc:       newCompany.Id,
+		Name:      newCompany.Name,
+		Employees: newCompany.Employees,
+	}
+
+	result := database.Create(&c)
+	logz.Debug(result)
+	if result.Error != nil {
+		logz.Debug(result.Error)
+		http.Error(w, err.Error(), http.StatusConflict)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(c)
 }
 
 func (b *Router) companyRoutes() {
+	database = b.DB.Data
 	b.r.HandleFunc("/register", register).Methods("POST")
 }
